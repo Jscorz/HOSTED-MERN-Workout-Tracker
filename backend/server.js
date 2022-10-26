1e91d3f2ffe1786a2ed48c555147bdadbae88ef1
Justin Scorzafava <jkscorzafava@gmail.com>
2022-10-25 19:37:49 -0600

    added cors package to backend code

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
+const cors = require("cors");
 
 const app = express();
 
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
 	console.log(req.path, req.method);
@@ -23,10 +25,7 @@ mongoose
 	.connect(process.env.MONGO_URI)
 	.then(() => {
 		app.listen(process.env.PORT, () => {
-			console.log(
-				"connected to db & listening on port",
-				process.env.PORT
-			);
+			console.log("connected to db & listening on port", process.env.PORT);
 		});
 	})
 	.catch((error) => {
